520337def972b450397604c5d7e60ac54e4fc753
AndRomMan <demromdeveloper@gmail.com>
2020-11-16 01:31:01 +0300

    fix(html, js): исправлены ошибки в поле input[type=tel]
    1 исправлено неправильное ограничение количества символов
    2 исправлен placeholder

--- build/js/main.js
+++ build/js/main.js
@@ -3,6 +3,12 @@
 (function () {
   window.navbar.activatingMenu();
   window.contactsMap.activatingGoogleMap();
+
+  // var element = document.querySelector('input[type="tel"]');
+  // var maskOptions = {
+  //   mask: '+{7}(000) 000 0000',
+  // };
+  // var mask = IMask(element, maskOptions);
 })();
 
 //# sourceMappingURL=main.js.map
