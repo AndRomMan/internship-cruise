ec80395e13522c8ed70d9296eb484897f0bf823b
AndRomMan <demromdeveloper@gmail.com>
2020-11-16 21:26:38 +0300

    fix(scss, js): исправлены расхождения с макетом
    исправлено расхождение с макетом секций header, promo, description
    при отключенном JS

--- source/js/navbar.js
+++ source/js/navbar.js
@@ -6,12 +6,22 @@
   var menuToggle = document.querySelector('.toggle');
   var iconBurger = document.querySelector('.toggle__icon--burger');
   var iconClose = document.querySelector('.toggle__icon--close');
+  var promoWrapper = document.querySelector('.promo-wrapper');
+  var descriptionWrapper = document.querySelector('.description-wrapper');
 
   var navbarList = document.querySelector('.nav-list');
   var logoLight = document.querySelector('.logo-light');
   var logoDark = document.querySelector('.logo-dark');
 
   var siteHeaderBack = document.querySelector('.site-header__back');
+
+  if (promoWrapper) {
+    promoWrapper.classList.add('promo-wrapper--js');
+  }
+
+  if (descriptionWrapper) {
+    descriptionWrapper.classList.add('description-wrapper--js');
+  }
 
   if (navbarList) {
     navbarList.classList.add('nav-list--closed');
