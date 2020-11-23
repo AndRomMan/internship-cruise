304877e30733b85ab427f26b57a39d981492faac
AndRomMan <demromdeveloper@gmail.com>
2020-11-23 16:34:05 +0300

    fix(html, scss, js): реализован на js обработчик открытия карточек

---
+++ build/js/cruises-item.js
@@ -0,0 +1,28 @@
+'use strict';
+// cruises-item.js
+
+(function () {
+  var cruisesCards = document.querySelectorAll('.cruises-item');
+  var cruisesCardsLength = cruisesCards.length;
+
+  function cruiseCardClickHandler() {
+    this.classList.toggle('cruises-item--js');
+  }
+
+  function cruisesCardsIni() {
+    if (cruisesCards) {
+      for (var key = 0; key < cruisesCardsLength; key++) {
+        cruisesCards[key].classList.add('cruises-item--js');
+        cruisesCards[key].addEventListener('click', cruiseCardClickHandler);
+      }
+    }
+  }
+
+  cruisesCardsIni();
+
+  window.cruisesItem = {
+    // cruisesCardsIni: cruisesCardsIni,
+  };
+})();
+
+//# sourceMappingURL=cruises-item.js.map
